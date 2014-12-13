873910a62aff86047ad08f0c5e5088d27f72a9b5
Ladvien <design@ubermentis.com>
2014-12-13 08:21:55 -0600

    v20
    Forward declared, working on RX function

--- main.c
+++ main.c
@@ -46,6 +46,28 @@ The smallest amount of data we may write to Flash is 256 bytes (one page).
 
 *////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+//////////////////// Forward Declaration ////////////////////////////////////////////////////////////
+
+// Output files, for debugging purposes.
+void writeUUEDataTofile(unsigned char fileData_Hex_String[], int hexDataCharCount);
+void writeHexDataTofile(unsigned char fileData_Hex_String[], int hexDataCharCount, unsigned char byteCount[], int hexFileLineCount);
+
+// FTDI
+static FILE *open_file ( char *file, char *mode );
+void fileSizer();
+void rx();
+
+// Data Handling
+struct hexFile hexFileToCharArray();
+int readByte();
+void clearSpecChar();
+int Hex2Ascii(unsigned char hexValue);
+int Hex2Int(unsigned char c);
+static unsigned char Ascii2Hex(unsigned char c);
+struct UUE_Data UUencode();
+
+//////////////////// Variables and Defines ////////////////////////////////////////////////////////
+
 //#define PIN_TX  0x01  /* Orange wire on FTDI cable */
 //#define PIX_RX  0x02  /* Yellow */
 //#define PIN_RTS 0x04  /* Green */
@@ -60,6 +82,13 @@ The smallest amount of data we may write to Flash is 256 bytes (one page).
 //Serial Port handle.
 FT_HANDLE handle = NULL;
 FT_STATUS FT_status;
+//Used by FTD2XX
+DWORD EventDWord;
+DWORD TxBytes;
+DWORD bytes;
+DWORD RxBytes;
+DWORD BytesReceived;
+char RxBuffer[256];
 
 //File to be loaded.	
 FILE *fileIn;
@@ -96,8 +125,122 @@ struct UUE_Data{
 	int UUE_Encoded_String_Index;
 };
 
+
+
 ///////////////////// PROGRAM ////////////////////////////
 ////////////////////// START /////////////////////////////
+
+int main(int argc, char *argv[])
+{
+	// Stores hexFile data.
+	struct hexFile hexFile;
+
+	// Stores UUE data.
+	struct UUE_Data UUE_Data;
+
+	//For setting state of DTR/CTS.
+	unsigned char DTR_Switch;
+	unsigned char CTS_Switch;
+
+	//If the user fails to give us two arguments yell at him.	
+	if ( argc != 2 ) {
+		fprintf ( stderr, "Usage: %s <readfile1>\n", argv[0] );
+		exit ( EXIT_FAILURE );
+	}
+
+	//Open file using command-line info; for reading.
+	fileIn = open_file (argv[1], "rb" );
+	
+	// Initialize, open device, set bitbang mode w/5 outputs
+	if(FT_Open(0, &handle) != FT_OK) {
+		puts("Can't open device");
+		return 1;
+	}
+
+	// Holds FTDI handle status.
+	FT_status = FT_Open(0, &handle);
+
+	//Set up pins we will use.
+	//FT_SetBitMode(handle, PIN_DTR | PIN_CTS, 1);
+	//Setup serial port, even though we are banging.
+	FT_SetBaudRate(handle, 9600);  //* Actually 9600 * 16
+
+	//Sizes file to be used in data handling.
+	fileSizer();
+	
+	//Convert file to one long char array.
+	hexFile = hexFileToCharArray();
+
+	//int dataLinesInFile = sizeof(hexFile.hexFileLineCount);
+	printf("%i\n", hexFile.hexFileLineCount);
+
+	// Write hex string back to a file.  Used for debugging.
+	writeHexDataTofile(hexFile.fileData_Hex_String, hexFile.fileData_Hex_String_Size, hexFile.fhexByteCount, hexFile.hexFileLineCount);
+	
+	// Convert hex data to UUE.
+	UUE_Data = UUencode();
+
+	//Used for indexing data bytes in for-loop.
+	int dataPerLineIndex = 0;
+	for (int totalUUEData_Index = 0; totalUUEData_Index < hexFile.fileData_Hex_String_Size; totalUUEData_Index)
+	{
+		for (int i = 0; i < 60; i++)
+		{
+			printf(" 0x%2x", hexFile.fileData_Hex_String[totalUUEData_Index]);
+			totalUUEData_Index++;
+		}
+		printf("\nEND PAGE\n\n");
+	}
+
+	unsigned char lineReturn = '\r';
+	//Prints out each byte, one at a time.
+
+	/*
+	for (int i = 0; i < UUE_Data.UUE_Encoded_String_Index; i++){
+		//This should print just data (ie, no Start Code, Byte Count, Address, Record type, or Checksum).
+		FTWrite_Check = FT_Write(handle, &UUE_Data.UUE_Encoded_String[i], (DWORD)sizeof(UUE_Data.UUE_Encoded_String[i]), &bytes);
+		//FTWrite_Check = FT_Write(handle, &lineReturn, 1, &bytes);
+		Sleep(5);
+	}	
+	*/
+
+	// HM-11-A sends "AT+PIO30"
+	// The HM-11-B PIO3 will go low, which sets the LPC1114 ISP MODE to LOW.  
+	// HM-11-A sends "AT+PIO20"
+	// Then, the HM-11-B PIO2 goes LOW for ~5uS.  This will reset the LPC.
+	// As the LPC comes back up it checks the ISP MODE pin and sees it LOW.
+	// HM-11-A sends "AT+PIO31" waits ~100mS then sends "AT+PIO21"
+	// The HM-11-B PIO3 and PIO2 will go HIGH.
+	// The LPC enters ISP mode.
+	// The HM-11 PIO2 & PIO3 go HIGH.
+	// The program is uploaded to the LPC.
+	// HM-11-A sends "AT+PIO20"
+	// HM-11-B PIO2 goes LOW, resetting the LPC.
+	// HM-11-A sends "AT+PIO21"
+	// HM-11-B PIO2 goes HIGH and the LPC runs the uploaded program.
+		
+	//char lower_Isp[] = "AT+RESET";
+	//FT_status = FT_Write(handle, &lower_Isp, ((DWORD)sizeof(lower_Isp)-1), &bytes);
+	//printf("%i\n", bytes);
+
+	rx();
+
+	//Let's close the serial port.
+	if(FT_Close(handle) != FT_OK) {
+		puts("Can't close device");
+		return 1;
+	}
+
+	//Close files.
+	fclose ( fileIn );
+	fclose ( UUEDataFile );
+	fclose ( hexDataFile );
+} // END PROGRAM
+
+
+
+///////////// FTDI  //////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
 
 //Open file for reading, function.
 static FILE *open_file ( char *file, char *mode )
@@ -112,6 +255,215 @@ static FILE *open_file ( char *file, char *mode )
   return fileOpen;
 }
 
+void fileSizer()
+{
+	while((fgetc (fileIn)) != EOF)
+	{
+		fileSize++;
+	}
+	rewind(fileIn);
+}
+
+void rx()
+{
+
+	/////////////// MUST ADD TIMEOUT FOR THIS TO WORK.
+	while (1)
+	{
+
+		FT_GetStatus(handle,&RxBytes,&TxBytes,&EventDWord);
+
+		if (RxBytes > 0) {
+			FT_status = FT_Read(handle, RxBuffer, RxBytes, &BytesReceived);
+			if (FT_status == FT_OK) {
+				printf("RX: %s\n", RxBuffer);
+
+				//break;
+			}
+			else {
+				printf("RX buffer empty.\n");
+			}
+		}
+		else {
+			break;
+		}
+		Sleep(10);
+	}			
+}
+
+///////////// Debugging //////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+void writeUUEDataTofile(unsigned char fileData_Hex_String[], int hexDataCharCount)
+{
+	UUEDataFile = open_file ("uueFile.uue", "w" );
+	if (UUEDataFile == NULL) {
+		printf("I couldn't open uueFile.uue for writing.\n");
+		exit(0);
+	}
+}
+
+void writeHexDataTofile(unsigned char fileData_Hex_String[], int hexDataCharCount, unsigned char byteCount[], int hexFileLineCount)
+{
+	
+	hexDataFile = open_file ("hexFile.hex", "w" );
+	if (hexDataFile == NULL) {
+		printf("I couldn't open hexFile.hex for writing.\n");
+		exit(0);
+	}
+	
+	for (int totalDataIndex = 0; totalDataIndex < hexDataCharCount; totalDataIndex)
+	{
+		for (int lineIndex = 0; lineIndex < hexFileLineCount; ++lineIndex)
+		{
+			int bytesThisLine = (byteCount[lineIndex]);
+			for (int i = 0; i < bytesThisLine && totalDataIndex < hexDataCharCount; ++i)  //need to change 16 to byteCount.
+			{
+				if ((fileData_Hex_String[totalDataIndex]) != 0x00)
+				{
+					fprintf(hexDataFile, "%02X", fileData_Hex_String[totalDataIndex]);
+				}
+				else
+				{
+					fprintf(hexDataFile, "00", fileData_Hex_String[totalDataIndex]);
+				}
+				totalDataIndex++;
+			}
+		fprintf(hexDataFile, "\n");
+		}
+
+	}
+}
+
+
+////////////////////////////// Data Handling (Conversion, etc.) //////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+//Copied in from lpc21isp.c
+static unsigned char Ascii2Hex(unsigned char c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (unsigned char)(c - '0');
+	}
+	if (c >= 'A' && c <= 'F')
+	{
+		return (unsigned char)(c - 'A' + 10);
+	}
+	if (c >= 'a' && c <= 'f')
+	{
+        return (unsigned char)(c - 'A' + 10);
+	}
+	//printf("\n !!! Bad Character: 0x%2x in file at totalCharsRead=%d !!!\n\n", c, totalCharsRead);
+	return 0;  // this "return" will never be reached, but some compilers give a warning if it is not present
+} 
+
+int Hex2Int(unsigned char c)
+{
+	int first = c / 16 - 3;
+	int second = c % 16;
+	int result = first*10 + second;
+	//if(result > 9) result--;
+	return result;
+}
+
+int Hex2Ascii(unsigned char hexValue)
+{
+	unsigned char c = hexValue >>= 4;
+	hexValue <<= 4;
+	unsigned char d = hexValue >>= 4;
+
+	int high = Hex2Int(c) * 16;
+	int low = Hex2Int(d);
+	return high+low;
+}
+
+void clearSpecChar()
+{
+	//Removes CR, LF, ':'  --Bdk6's
+	while (charToPut == '\n' || charToPut == '\r' || charToPut ==':'){
+		(charToPut = fgetc (fileIn));
+		totalCharsRead++;
+	}
+}
+
+int readByte(){
+	//Holds combined nibbles.
+	unsigned char hexValue;
+	
+	//Get first nibble.
+	charToPut = fgetc (fileIn);
+	clearSpecChar();
+	//Put first nibble in.
+	hexValue = (Ascii2Hex(charToPut));
+	//Slide the nibble.
+	hexValue <<= 4;
+	//Put second nibble in.
+	charToPut = fgetc (fileIn);
+	clearSpecChar();
+	//Put the nibbles together.
+	hexValue |= (Ascii2Hex(charToPut));
+	//Return the byte.
+	totalCharsRead=totalCharsRead+2;
+	return hexValue;
+}
+
+
+//Convert file to one long char array.
+struct hexFile hexFileToCharArray()
+{
+	//To count through all characters in the file.
+	int i = 0;
+	int hexDataIndex = 0;
+	int charsThisLine;
+	//Holds line count.
+	
+	struct hexFile hexFile;
+
+	//Loop through each character until EOF.
+	while(totalCharsRead  < fileSize){
+		
+		//BYTE COUNT
+		hexFile.fhexByteCount[i] = readByte();
+		
+		//ADDRESS1 //Will create an 8 bit shift. --Bdk6's
+		hexFile.fhexAddress1[i] = readByte();
+		
+		//ADDRESS2
+		hexFile.fhexAddress2[i] = readByte();
+		
+		//addressCombine();
+		
+		//RECORD TYPE
+		hexFile.fhexRecordType[i] = readByte();	
+		
+		//Throws the byte count (data bytes in this line) into an integer.
+		charsThisLine = hexFile.fhexByteCount[i];
+
+		//////// DATA ///////////////////
+		while (hexDataIndex != charsThisLine && totalCharsRead  < fileSize && charsThisLine != 0x00)
+		{
+			//Store the completed hex value in the char array.
+			hexFile.fileData_Hex_String[hexFile.fileData_Hex_String_Size] = readByte();
+			
+			//Index for data.
+			hexFile.fileData_Hex_String_Size++;
+			//Index for loop.
+			hexDataIndex++;
+		}
+		//Reset loop index.
+		hexDataIndex=0;
+		
+		//////// CHECK SUM //////////////
+		if (charToPut != 0xFF){
+			hexFile.fhexCheckSum[i] = readByte();
+		}
+
+		hexFile.hexFileLineCount++;
+		i++;
+	}
+	return hexFile;
+}
 
 struct UUE_Data UUencode()
 {
@@ -171,304 +523,3 @@ struct UUE_Data UUencode()
 	UUE_Data.UUE_Encoded_String_Index = UUE_Data.UUE_Encoded_String_Index - UUE_Data.paddedIndex;
 	return UUE_Data;
 }
-
-void fileSizer()
-{
-	while((fgetc (fileIn)) != EOF)
-	{
-		fileSize++;
-	}
-	rewind(fileIn);
-}
-
-/***************************** Ascii2Hex ********************************/
-//Copied in from lpc21isp.c
-static unsigned char Ascii2Hex(unsigned char c)
-{
-    if (c >= '0' && c <= '9')
-    {
-        return (unsigned char)(c - '0');
-    }
-    if (c >= 'A' && c <= 'F')
-    {
-        return (unsigned char)(c - 'A' + 10);
-    }
-    if (c >= 'a' && c <= 'f')
-    {
-        return (unsigned char)(c - 'A' + 10);
-    }
-	//printf("\n !!! Bad Character: 0x%2x in file at totalCharsRead=%d !!!\n\n", c, totalCharsRead);
-    return 0;  // this "return" will never be reached, but some compilers give a warning if it is not present
-} 
-
-int Hex2Int(unsigned char c)
-{
-	int first = c / 16 - 3;
-    int second = c % 16;
-    int result = first*10 + second;
-    //if(result > 9) result--;
-    return result;
-}
-
-int Hex2Ascii(unsigned char hexValue)
-{
-	unsigned char c = hexValue >>= 4;
-	hexValue <<= 4;
-	unsigned char d = hexValue >>= 4;
-
-    int high = Hex2Int(c) * 16;
-	int low = Hex2Int(d);
-	return high+low;
-}
-
-void clearSpecChar()
-{
-	//Removes CR, LF, ':'  --Bdk6's
-	while (charToPut == '\n' || charToPut == '\r' || charToPut ==':'){
-		(charToPut = fgetc (fileIn));
-		totalCharsRead++;
-	}
-}
-
-int readByte(){
-	//Holds combined nibbles.
-	unsigned char hexValue;
-	
-	//Get first nibble.
-	charToPut = fgetc (fileIn);
-	clearSpecChar();
-	//Put first nibble in.
-	hexValue = (Ascii2Hex(charToPut));
-	//Slide the nibble.
-	hexValue <<= 4;
-	//Put second nibble in.
-	charToPut = fgetc (fileIn);
-	clearSpecChar();
-	//Put the nibbles together.
-	hexValue |= (Ascii2Hex(charToPut));
-	//Return the byte.
-	totalCharsRead=totalCharsRead+2;
-	return hexValue;
-}
-
-
-//Convert file to one long char array.
-struct hexFile hexFileToCharArray()
-{
-	//To count through all characters in the file.
-	int i = 0;
-	int hexDataIndex = 0;
-	int charsThisLine;
-	//Holds line count.
-	
-	
-	struct hexFile hexFile;
-
-	//Loop through each character until EOF.
-	while(totalCharsRead  < fileSize){
-		
-		//BYTE COUNT
-		hexFile.fhexByteCount[i] = readByte();
-		
-		//ADDRESS1 //Will create an 8 bit shift. --Bdk6's
-		hexFile.fhexAddress1[i] = readByte();
-		
-		//ADDRESS2
-		hexFile.fhexAddress2[i] = readByte();
-		
-		//addressCombine();
-		
-		//RECORD TYPE
-		hexFile.fhexRecordType[i] = readByte();	
-		
-		//Throws the byte count (data bytes in this line) into an integer.
-		charsThisLine = hexFile.fhexByteCount[i];
-
-		//////// DATA ///////////////////
-		while (hexDataIndex != charsThisLine && totalCharsRead  < fileSize && charsThisLine != 0x00)
-		{
-			//Store the completed hex value in the char array.
-			hexFile.fileData_Hex_String[hexFile.fileData_Hex_String_Size] = readByte();
-			
-			//Index for data.
-			hexFile.fileData_Hex_String_Size++;
-			//Index for loop.
-			hexDataIndex++;
-		}
-		//Reset loop index.
-		hexDataIndex=0;
-		
-		//////// CHECK SUM //////////////
-		if (charToPut != 0xFF){
-			hexFile.fhexCheckSum[i] = readByte();
-		}
-
-		hexFile.hexFileLineCount++;
-		i++;
-		
-		
-	}
-	
-	
-	return hexFile;
-}
-
-void writeHexDataTofile(unsigned char fileData_Hex_String[], int hexDataCharCount, unsigned char byteCount[], int hexFileLineCount)
-{
-	
-	hexDataFile = open_file ("hexFile.hex", "w" );
-	if (hexDataFile == NULL) {
-    	printf("I couldn't open hexFile.hex for writing.\n");
-        exit(0);
-    }
-	
-	for (int totalDataIndex = 0; totalDataIndex < hexDataCharCount; totalDataIndex)
-	{
-		for (int lineIndex = 0; lineIndex < hexFileLineCount; ++lineIndex)
-		{
-			int bytesThisLine = (byteCount[lineIndex]);
-			for (int i = 0; i < bytesThisLine && totalDataIndex < hexDataCharCount; ++i)  //need to change 16 to byteCount.
-			{
-				if ((fileData_Hex_String[totalDataIndex]) != 0x00)
-				{
-					fprintf(hexDataFile, "%02X", fileData_Hex_String[totalDataIndex]);
-				}
-				else
-				{
-					fprintf(hexDataFile, "00", fileData_Hex_String[totalDataIndex]);
-				}
-				totalDataIndex++;
-			}
-		fprintf(hexDataFile, "\n");
-		}
-
-	}
-}
-
-void writeUUEDataTofile(unsigned char fileData_Hex_String[], int hexDataCharCount)
-{
-	UUEDataFile = open_file ("uueFile.uue", "w" );
-	if (UUEDataFile == NULL) {
-    	printf("I couldn't open uueFile.uue for writing.\n");
-    	exit(0);
-    }
-}
-
-
-
-int main(int argc, char *argv[])
-{
-
-
-	// Stores hexFile data.
-	struct hexFile hexFile;
-
-	// Stores UUE data.
-	struct UUE_Data UUE_Data;
-
-	//For setting state of DTR/CTS.
-    unsigned char DTR_Switch;
-	unsigned char CTS_Switch;
-
-	//Used by FTD2XX
-    DWORD bytes;
-
-
-
-    //If the user fails to give us two arguments yell at him.	
-	if ( argc != 2 ) {
-		fprintf ( stderr, "Usage: %s <readfile1>\n", argv[0] );
-		exit ( EXIT_FAILURE );
-	}
-
-	//Open file using command-line info; for reading.
-	fileIn = open_file (argv[1], "rb" );
-	
-	// Initialize, open device, set bitbang mode w/5 outputs
-    if(FT_Open(0, &handle) != FT_OK) {
-        puts("Can't open device");
-        return 1;
-    }
-
-    // Holds FTDI handle status.
-	FT_status = FT_Open(0, &handle);
-
-	//Set up pins we will use.
-	//FT_SetBitMode(handle, PIN_DTR | PIN_CTS, 1);
-	//Setup serial port, even though we are banging.
-    FT_SetBaudRate(handle, 9600);  //* Actually 9600 * 16
-
-	//Sizes file to be used in data handling.
-	fileSizer();
-	
-	//Convert file to one long char array.
-	hexFile = hexFileToCharArray();
-
-	//int dataLinesInFile = sizeof(hexFile.hexFileLineCount);
-	printf("%i\n", hexFile.hexFileLineCount);
-
-	// Write hex string back to a file.  Used for debugging.
-	writeHexDataTofile(hexFile.fileData_Hex_String, hexFile.fileData_Hex_String_Size, hexFile.fhexByteCount, hexFile.hexFileLineCount);
-	
-	// Convert hex data to UUE.
-	UUE_Data = UUencode();
-
-	//Used for indexing data bytes in for-loop.
-	int dataPerLineIndex = 0;
-	for (int totalUUEData_Index = 0; totalUUEData_Index < hexFile.fileData_Hex_String_Size; totalUUEData_Index)
-	{
-		for (int i = 0; i < 60; i++)
-		{
-			printf(" 0x%2x", hexFile.fileData_Hex_String[totalUUEData_Index]);
-			totalUUEData_Index++;
-		}
-		printf("\nEND PAGE\n\n");
-	}
-
-	unsigned char lineReturn = '\r';
-	//Prints out each byte, one at a time.
-
-	/*
-	for (int i = 0; i < UUE_Data.UUE_Encoded_String_Index; i++){
-		//This should print just data (ie, no Start Code, Byte Count, Address, Record type, or Checksum).
-		FTWrite_Check = FT_Write(handle, &UUE_Data.UUE_Encoded_String[i], (DWORD)sizeof(UUE_Data.UUE_Encoded_String[i]), &bytes);
-		//FTWrite_Check = FT_Write(handle, &lineReturn, 1, &bytes);
-		Sleep(5);
-	}	
-	*/
-
-	
-// HM-11-A sends "AT+PIO30"
-// The HM-11-B PIO3 will go low, which sets the LPC1114 ISP MODE to LOW.  
-// HM-11-A sends "AT+PIO20"
-// Then, the HM-11-B PIO2 goes LOW for ~5uS.  This will reset the LPC.
-// As the LPC comes back up it checks the ISP MODE pin and sees it LOW.
-// HM-11-A sends "AT+PIO31" waits ~100mS then sends "AT+PIO21"
-// The HM-11-B PIO3 and PIO2 will go HIGH.
-// The LPC enters ISP mode.
-// The HM-11 PIO2 & PIO3 go HIGH.
-// The program is uploaded to the LPC.
-// HM-11-A sends "AT+PIO20"
-// HM-11-B PIO2 goes LOW, resetting the LPC.
-// HM-11-A sends "AT+PIO21"
-// HM-11-B PIO2 goes HIGH and the LPC runs the uploaded program.
-	
-	char lower_Isp[] = "AT+RESET";
-
-	FT_status = FT_Write(handle, &lower_Isp, ((DWORD)sizeof(lower_Isp)-1), &bytes);
-	printf("%i\n", bytes);
-	Sleep(5);
-
-	
-
-	//Let's close the serial port.
-	if(FT_Close(handle) != FT_OK) {
-        puts("Can't close device");
-        return 1;
-    }
-
-	//Close files.
-	fclose ( fileIn );
-	fclose ( UUEDataFile );
-	fclose ( hexDataFile );
-} // END PROGRAM

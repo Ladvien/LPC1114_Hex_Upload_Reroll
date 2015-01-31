59160ab25e68cb179e9e1540015299a44b8186d9
Thomas Brittain <design@ubermentis.com>
2015-01-31 09:19:08 -0600

    Grinding rewrite of create_sector_to_write()

--- main.c
+++ main.c
@@ -212,7 +212,7 @@ void startScreen();
 // Write to RAM.
 struct Write_to_RAM write_two_pages_to_ram(struct Write_to_RAM write_to_RAM_local, struct Data data_local, uint8_t * uue_pages_or_scrap_array, uint32_t * ram_address, int uue_pages_or_scrap_char_count, int pages_or_scrap_check_sum, int number_of_bytes_to_write, int hex_data_array_size, int * bytes_written);
 //struct Write_to_RAM uue_create_pages_or_scrap(struct Write_to_RAM write_to_RAM_local, struct Data data_local, uint8_t * uue_pages_or_scrap_array, int * pages_or_scrap_check_sum, int * number_of_bytes_to_write, int * bytes_written);
-struct Write_to_RAM prepare_to_write_sector(struct Write_to_RAM write_to_RAM_local, struct Data data_local);
+struct Write_to_RAM prepare_write_sector(struct Write_to_RAM write_to_RAM_local, struct Data data_local);
 void convert_32_hex_address_to_string(uint32_t address, uint8_t * address_as_string);
 
 // Timers
@@ -322,7 +322,7 @@ int main(int argc, uint8_t *argv[])
 	while(bytes_written < data.HEX_array_size)
 	{
 		// UUEncode 2 pages (512 bytes).  Returns UUE character count (~1033)
-		write_to_RAM = prepare_to_write_sector(write_to_RAM, data);
+		write_to_RAM = prepare_write_sector(write_to_RAM, data);
 		for (int i = 0; i < uue_pages_or_scrap_char_count; ++i)
 		{
 			printf("%C ", uue_pages_or_scrap_array[i]);
@@ -1412,7 +1412,7 @@ struct Write_to_RAM write_two_pages_to_ram(struct Write_to_RAM write_to_RAM_loca
 
 
 
-struct Write_to_RAM prepare_to_write_sector(struct Write_to_RAM write_to_RAM_local, struct Data data_local)
+struct Write_to_RAM prepare_write_sector(struct Write_to_RAM write_to_RAM_local, struct Data data_local)
 {
 	// 1. Load hex data into array.
 	// 2. UUEencode the chunk & get UUE char count.
@@ -1441,25 +1441,49 @@ struct Write_to_RAM prepare_to_write_sector(struct Write_to_RAM write_to_RAM_loc
 	*/
 	//data_local.HEX_array
 	//data_local.HEX_array_size
-
+	//printf("BLAH! 1\n");
 	// Load 4096 bytes.
 	while(half_page_index < 32){
-		
-		while(buffer_index != write_to_RAM_local.bytes_written)
+		//printf("BLAH! 2\n");
+		if (write_to_RAM_local.number_of_bytes_to_write[half_page_index] < 128)
 		{
-			data_array_buffer[buffer_index] = data_local.HEX_array[load_data_index];
-			printf("%02X ", data_array_buffer[buffer_index]);			
-			load_data_index++;
-			buffer_index++;
+			//printf("BLAH! 3\n");
+			while(buffer_index != write_to_RAM_local.number_of_bytes_to_write[half_page_index])
+			{
+				printf("BLAH! 4\n");
+				data_array_buffer[buffer_index] = data_local.HEX_array[load_data_index];
+				printf("HERE %02X ", data_array_buffer[buffer_index]);			
+				load_data_index++;
+				buffer_index++;
+			}
+
+			write_to_RAM_local.number_of_bytes_to_write[half_page_index] = buffer_index;
+			
+			// 2. UUEencode the chunk & get UUE char count.
+			write_to_RAM_local.UUE_char_count = UUEncode(UUE_char_array_buffer, data_array_buffer, buffer_index);
+
+			// 3. Create checksum for encoded pages.
+			write_to_RAM_local.data_to_write_check_sum[half_page_index] = check_sum(data_array_buffer, buffer_index);
+		}
+		else
+		{
+			while(buffer_index != write_to_RAM_local.bytes_written)
+			{
+				data_array_buffer[buffer_index] = data_local.HEX_array[load_data_index];
+				printf("HERE %02X ", data_array_buffer[buffer_index]);			
+				load_data_index++;
+				buffer_index++;
+			}
+
+			write_to_RAM_local.number_of_bytes_to_write[half_page_index] = buffer_index;
+			
+			// 2. UUEencode the chunk & get UUE char count.
+			write_to_RAM_local.UUE_char_count = UUEncode(UUE_char_array_buffer, data_array_buffer, buffer_index);
+
+			// 3. Create checksum for encoded pages.
+			write_to_RAM_local.data_to_write_check_sum[half_page_index] = check_sum(data_array_buffer, buffer_index);
 		}
 
-		write_to_RAM_local.number_of_bytes_to_write[half_page_index] = buffer_index;
-		
-		// 2. UUEencode the chunk & get UUE char count.
-		write_to_RAM_local.UUE_char_count = UUEncode(UUE_char_array_buffer, data_array_buffer, buffer_index);
-
-		// 3. Create checksum for encoded pages.
-		write_to_RAM_local.data_to_write_check_sum[half_page_index] = check_sum(data_array_buffer, buffer_index);
 
 		// 1. Load chunk into array.
 		for (int i = 0; i < 128; ++i)

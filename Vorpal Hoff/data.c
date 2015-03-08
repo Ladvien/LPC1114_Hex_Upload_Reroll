d1e61a493f0319ba9c8b72645d7073f5fee1013f
Ladvien <design@ubermentis.com>
2015-03-08 17:25:26 -0500

    Aesthetics

--- Vorpal Hoff/data.c
+++ Vorpal Hoff/data.c
@@ -47,27 +47,28 @@ int hex_file_to_array(FILE * file, uint8_t hex_data[])
 	// 2. Read a line. From ':' to '\n'
 	// 3. Parse a line. 
 	//   Repeat for all lines.
-	int total_chars_read = 0;
 
 	// Data per line.
 	uint8_t line_of_data[32];
 	long int combined_address[4096];
 
-	// Indexes
+	// Indices and counters
 	int hex_line_index = 0;
-	// Line dimesions
 	int chars_this_line = 0;
-
+	int total_chars_read = 0;
 	// How many lines in the hexfile?
 	int hex_lines_in_file = 0;
 	int bytes_this_line[4096];
 
+	// Let's count how many lines are in this file.
 	hex_lines_in_file = hex_file_line_count(file);
 
+	// Indices for parsing.
 	int line_index = 0;
 	int byte_index = 0;
 	bool read_line_ok = false;
 
+	// Parse all lines in file.
 	while(line_index < hex_lines_in_file)
 	{
 		read_line_ok = read_line_from_hex_file(file, line_of_data, &combined_address[line_index], &bytes_this_line[line_index]);
@@ -86,6 +87,7 @@ int hex_file_to_array(FILE * file, uint8_t hex_data[])
 		line_index++;
 	}
 
+	// Print out parsed data.
 	int k = 0;
 	int j = 0;
 	int printed_bytes = 0;
